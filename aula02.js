e73caa842f892c04df8c826c08376493706fb19d
Edy_PJs <141849165+edy3333@users.noreply.github.com>
2024-01-07 00:05:33 -0300

    Add files via upload

---
+++ aula02.js
@@ -0,0 +1,67 @@
+ //Variáveis e Tipos Primitivos//
+
+/*EX... eu comprei um terreno e agora eu quero transformar esse terreno
+em estacionamento, então eu organizo por vagas e eu posso chamar essas vagas de VARIÁVEIS.
+em um shopping podemos perder um carro , Mas aí entra a Numeração, ex. vaga A1,vaga A2, vaga A3 etc....
+e se eu tiver que estacionar carros grande como caminhão, caminhote ou onibus.
+então eu posso chamar essas vagas que são as VARIÁVEIS de vaga C1, vagaC2, vaga C3 etc...
+Mas eu quero 6 vagas para motocicletas entao darei vaga M1, vaga M2, vaga M3, vaga M4, vaga M5, vaga M6.
+a partir de entao organizei espaços variados em nosso terreno.
+*/
+
+//COMO COLOCAR UM CARRO NA VAGA//
+/*Ex... vaga A1 recebe o carro1,
+quando eu quiser estacionar outro carro na vaga,eu retiro o carro A1=carro1 e recebo A1=carro2 --> ISSO SIGNIFICA QUE VAGA A1=carro2 RECEBEU CARRO2.
+Mas em caso de eu nao querer receber carro nenhum eu vou entao --> A1=NULL >> que significa que optei por deixar a minha vaga vazia .
+*/ 
+//o sinal de = significa recebe//
+
+/**OS COMPUTADORES FUNCIONAM ASSIM , NÃO QUE EU POSSA ESTACIONAR CARROS E SIM DADOS E ARMAZENAMENTOS .
+então temos que organizar espaços delimitados para receber valores. 
+*/
+
+//NOME DA VARIÁVEL//
+/* esses nomes podemos chamar de indentificadores e não pode escolher qualquer nome .
+EX.. os nomes podem começar LETRAS,$ ou _ 
+EX.. não pode começar com NUMEROS 
+EX.. sim, é possivel ultilizar LETRAS ou NUMEROS como CARRO1 ou SALARIO2
+EX.. também é possivel usar ACENTOS e SIMBOLOS
+EX..não pode conter ESPAÇOS
+EX.não pode PALAVRAS RESERVADAS*/
+
+//AULA SIMPLES COM NODE.JS//
+/*Welcome to Node.js v18.17.0.
+Type ".help" for more information.
+> var nome = "Edilene"
+undefined
+> var casa = "number-253"
+undefined
+> var status = "solteira"
+undefined
+> var ocuppacion = "estudante"
+undefined
+> nome
+'Edilene'
+> casa
+'number-253'
+> status
+'solteira'
+> ocuppacion
+'estudante'
+>*/
+
+//DICAS// 
+/*
+1-MAIUSCULAS e MINUSCULAS fazem sim diferença.
+2-usar NOMES COERENTES para as variaveis
+3-evitar ser um 'PROGRAMADOR ALFABETO' ou um 'PROGRAMADOR CONTADOR'
+*/
+
+//DATA TYPES//
+/* 1-NUMBER tem dois valores interno muito importante que são INFINITY e o NaN.
+2-STRING.
+3-BOOLEAN.
+4-NULL.
+5-UNDERFINED.
+6-OBJECT o modo interno é o ARRAY.
+7-FUNCTION um tipo primitivo na linguagem js*/ 
