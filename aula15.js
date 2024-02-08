08a700409476657553093be4ba6e61a92051f96e
Edy_PJs <141849165+edy3333@users.noreply.github.com>
2024-02-08 14:09:31 -0400

    Add files via upload

---
+++ aula15.js
@@ -0,0 +1,26 @@
+//ESTRUTURA DE REPETIÇÕES PARTE #2//
+
+/*var c = 1
+while (c <= 10){
+    console.log(c);
+    c++
+} */
+
+// USANDO DO //
+/*var c = 1
+do{
+    console.log(c);
+    c++
+} while (c<=10) */
+
+// SANDO FOR //
+/*for(var c = 1; c <= 10; c++){
+    console.log(c);
+}*/
+
+//FAZENDO UMAS ALTERAÇÕESZINHAS //
+console.log('Vai começar o show');
+for(var c = 1; c <= 10; c++){
+    console.log(c);
+} 
+console.log('Fim do Show');
