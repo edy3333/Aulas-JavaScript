77c41b445f9327803e19f1b79f501064d13cad44
Edy_PJs <141849165+edy3333@users.noreply.github.com>
2024-02-11 13:28:59 -0300

    Add files via upload

---
+++ aula18.js
@@ -0,0 +1,6 @@
+// e se eu quiser mostrar cada valor do elemento especifico do array ?//
+let vetor = [4,6,5,7,34,56]
+vetor.sort()
+for(let pos = 0; pos < vetor.length;pos++){
+    console.log(`a posição tem valor ${pos} tem o valor ${vetor[pos]}`);
+}
