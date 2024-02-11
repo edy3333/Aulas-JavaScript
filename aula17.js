d761ef1e0c12fd9273a4c15dd72e284429603b63
Edy_PJs <141849165+edy3333@users.noreply.github.com>
2024-02-10 21:55:49 -0300

    Add files via upload

---
+++ aula17.js
@@ -0,0 +1,15 @@
+let num = [4,99,5]
+num.sort()
+console.log(num);
+console.log(`nosso valor tem ${num.length} posições`)
+console.log(`no primeiro vetor é ${num[0]}`)
+
+//num[3]=6 = com isso eu adiciono o valor 6 na casa 3//
+
+//num.push(7) = com isso adiciono numero 7 na casa 4//
+
+//num.length = com isso saberei o comprimento de um elemento //
+
+//num.sort() = com isso colocarei os elementos em ordem crescente//
+
+
